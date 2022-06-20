b3a8086820779a34d4c4dd16ba91ceec83e4d753
hezirel <80560775+hezirel@users.noreply.github.com>
2022-06-20 18:18:41 +0200

    Working solution with fileupload middleware => review nodejs stream to do without

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fileUpload = require('express-fileupload');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -6,13 +7,28 @@ var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/api/fileanalyse", fileUpload());
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.route('/api/fileanalyse')
+    .post((req, res) => {
+      const file = req.files.upfile;
+      if (file) {
+        res.json({
+          name: file.name,
+          type: file.mimetype,
+          size: file.size
+        })
+      }
+      else {
+        res.json({
+          body: 'No request body'
+        })
+      }
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
